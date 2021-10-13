cb71c9b7ec6dfa1a97a3372135d71d522d05b589
jaeyoung <dlo3olbb@naver.com>
2021-10-13 17:11:56 +0900

    background color to BLUE

--- GitPractice/GitPractice/ViewController.swift
+++ GitPractice/GitPractice/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .orange
+        self.view.backgroundColor = .blue
     }
 
 
